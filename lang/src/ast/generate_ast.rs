6f2795d5c0743e79258ddaec8a78fc6a0696484f
jmaczan <jedrzejpawel@maczan.pl>
2022-10-31 20:47:09 +0100

    Minor generate_ast cleanup

--- lang/src/ast/generate_ast.rs
+++ lang/src/ast/generate_ast.rs
@@ -19,6 +19,7 @@ pub fn generate_ast(output_directory: &str) {
 
 fn define_ast(output_directory: &str, file_name: &str, types: Vec<&str>) {
     let output_directory_path = Path::new(output_directory);
+
     if !output_directory_path.exists() {
         DirBuilder::new()
             .recursive(true)
@@ -27,6 +28,7 @@ fn define_ast(output_directory: &str, file_name: &str, types: Vec<&str>) {
     }
 
     let mut file = File::create(output_directory_path.join(file_name)).unwrap();
+
     for ast_type in types {
         let ast_type_components = ast_type.split("=").collect::<Vec<&str>>();
         let name: &str = ast_type_components.get(0).unwrap().trim();
@@ -36,8 +38,8 @@ fn define_ast(output_directory: &str, file_name: &str, types: Vec<&str>) {
 }
 
 fn define_type(file: &mut File, name: &str, fields: &str) {
-    let b = format!("pub struct {name} {{");
-    writeln!(file, "{b}").unwrap();
+    let struct_signature = format!("pub struct {name} {{");
+    writeln!(file, "{struct_signature}").unwrap();
     for field in fields.split(",").collect::<Vec<&str>>() {
         writeln!(file, "{field},").unwrap();
     }
