c3a71ef878a6a0bf1ebfd3972dde35634f9162c9
jmaczan <jedrzejpawel@maczan.pl>
2023-01-07 22:15:42 +0100

    Recursive descent parser - work in progress

--- lang/src/ast/parser.rs
+++ lang/src/ast/parser.rs
@@ -1,6 +1,6 @@
 use crate::lexical_analysis::{TokenType, Token};
 
-use super::expression::{Binary, Expr, Literal};
+use super::expression::{Binary, Expr, Literal, Unary};
 
 struct Parser {
     pub tokens: Vec<Token>,
@@ -15,134 +15,140 @@ impl Parser {
         }
     }
 
-    fn expression() {
-        Self::equality()
+    fn expression(&self) -> Box<dyn Expr> {
+        Self::equality(self)
     }
 
-    fn equality() -> Box<dyn Expr> {
-        let mut expr = Self::comparison();
+    fn equality(&self) -> Box<dyn Expr> {
+        let mut expr = Self::comparison(self);
 
-        while Self::match_token(Vec::from([TokenType::BangEqual, TokenType::EqualEqual])) {
-            let operator = Self::previous();
-            let right = Self::comparison();
-            expr = Binary {
-                left: Box::new(expr),
+        while Self::match_token(self, Vec::from([TokenType::BangEqual, TokenType::EqualEqual])) {
+            let operator = Self::previous(self);
+            let right = Self::comparison(self);
+            expr = Box::new(Binary {
+                left: expr,
                 operator,
                 right
-            };
+            });
         }
 
-        Box::new(expr)
+        expr
     }
 
-    fn match_token(tokens: Vec<TokenType>) -> bool {
+    fn match_token(&self, tokens: Vec<TokenType>) -> bool {
         for token in tokens {
-            if Self::check(token) {
-                Self::advance();
-                true
+            if Self::check(self, token) {
+                Self::advance(self);
+                return true
             }
         }
 
         false
     }
 
-    fn check(token: TokenType) -> bool {
-        if Self::is_at_end() {
-            false
+    fn check(&self, token: TokenType) -> bool {
+        if Self::is_at_end(self) {
+            return false
         }
 
-        Self::peek().type == type
+        Self::peek(self) == token
     }
 
-    fn advance() -> TokenType {
-        if !Self::is_at_end() {
+    fn advance(&self) -> TokenType {
+        if !Self::is_at_end(self) {
             self.current = self.current + 1;
         }
 
-        Self::previous()
+        Self::previous(self)
     }
 
-    fn is_at_end() -> bool {
-        Self::peek().type == '\0'
+    fn is_at_end(&self) -> bool {
+        Self::peek(self) == TokenType::Eof
     }
 
     fn peek(&self) -> TokenType {
-        self.tokens[self.current]
+        match self.tokens.get(self.current as usize) {
+            Some(token) => token.token_type,
+            None => TokenType::Eof, // TODO - I might be wrong here and maybe either nothing should be returned or it should be a different value than Eof
+        }
     }
 
     fn previous(&self) -> TokenType {
-        self.tokens[self.current - 1]
+        match self.tokens.get((self.current - 1) as usize) {
+            Some(token) => token.token_type,
+            None => TokenType::Eof, // TODO - I might be wrong here and maybe either nothing should be returned or it should be a different value than Eof
+        }
     }
 
-    fn comparison(&self) {
-        let mut expr = Self::term();
+    fn comparison(&self) -> Box<dyn Expr> {
+        let mut expr = Self::term(self);
 
-        while match_token(Vec::from([TokenType::Greater, TokenType::GreaterEqual, TokenType::Less, TokenType::LessEqual])) {
+        while Self::match_token(self, Vec::from([TokenType::Greater, TokenType::GreaterEqual, TokenType::Less, TokenType::LessEqual])) {
             let operator = Self::previous(&self);
-            let right = Self::term();
-            expr = Binary {
-                left: Box::new(expr),
+            let right = Self::term(self);
+            expr = Box::new(Binary {
+                left: expr,
                 operator,
                 right
-            }
+            });
         }
 
         expr
     }
 
-    fn term(&self) {
-        let mut expr = Self::factor();
+    fn term(&self) -> Box<dyn Expr> {
+        let mut expr = Self::factor(self);
 
-        while match_token(Vec::from([TokenType::Minus, TokenType::Plus])) {
+        while Self::match_token(self, Vec::from([TokenType::Minus, TokenType::Plus])) {
             let operator = Self::previous(&self);
             let right = Self::factor(&self);
-            expr = Binary {
+            expr = Box::new(Binary {
                 left: expr,
                 operator,
                 right
-            }
+            })
         }
 
         expr
     }
 
-    fn factor(&self) {
+    fn factor(&self) -> Box<dyn Expr> {
         let expr = Self::unary(&self);
 
-        while match_token(Vec::from([TokenType::Slash, TokenType::Star])) {
+        while Self::match_token(self, Vec::from([TokenType::Slash, TokenType::Star])) {
             let operator = Self::previous(&self);
             let right = Self::unary(&self);
-            expr = Binary {
+            expr = Box::new(Binary {
                 left: expr,
                 operator,
                 right
-            }
+            });
         }
 
         expr
     }
 
-    fn unary(&self) {
-        if Self::match_token(Vec::from([TokenType::Bang, TokenType:: Minus])) {
+    fn unary(&self) -> Box<dyn Expr> {
+        if Self::match_token(self, Vec::from([TokenType::Bang, TokenType:: Minus])) {
             let operator = Self::previous(&self);
             let right = Self::unary(&self);
-            Unary {
+            return Box::new(Unary {
                 operator,
                 right
-            }
+            })
         }
 
         Self::primary(&self)
     }
 
-    fn primary(&self) {
-        if Self::match_token(Vec::from([TokenType::False])) {
-            Literal {
+    fn primary(&self) -> Box<dyn Expr> {
+        if Self::match_token(self, Vec::from([TokenType::False])) {
+            return Box::new(Literal {
                 value: "false".to_string()
-            }
+            })
         }
 
-        if Self::match_token(Vec::from([TokenType::False])) {
+        if Self::match_token(self, Vec::from([TokenType::False])) {
             // TODO
         }
     }
